5f8db73e48858b3a4aa620f4b5cfb555d68fd41a
BushraMateen <bushra.mateen95@outlook.com>
2022-08-15 21:31:18 +0530

    created table

--- assestment-2/Table.js
+++ assestment-2/Table.js
@@ -1,36 +1,36 @@
 	
 var list = [
     {
-        "machine_name": "Auto Winding Machine",
-        "machine_id": "machine001",
-        "current": 0,
-        "voltage": 0,
-        "power_factor": 0,
+        "machine_name":"Auto Winding Machine",
+        "machine_id":"machine001",
+        "current":0,
+        "voltage":0,
+        "power_factor":0,
         "active_power": 0.0,
-        "apparent_power": 0.0,
-        "reactive_power": 0.0,
-        "daily_energy": 140,
-        "monthly_energy": 0,
-        "yearly_energy": 0,
-        "idle_daily": 0,
-        "idle_monthly": 0,
-        "idle_yearly": 150
-    },
-    {
-        "machine_name": "Boiler Machine",
-        "machine_id": "machine004",
-        "current": 0,
-        "voltage": 0,
-        "power_factor": 0,
-        "active_power": 0.0,
-        "apparent_power": 0.0,
+        "apparent_power" :0.0,
         "reactive_power": 0.0,
         "daily_energy": 0,
-        "monthly_energy": 0,
-        "yearly_energy": 0,
-        "idle_daily": 0,
-        "idle_monthly": 0,
-        "idle_yearly": 100
+        "monthly_energy" :0,
+        "yearly_energy" : 0,
+        "idle_daily" : 0,
+        "idle_monthly" :0,
+        "idle_yearly" : 0    
+    },
+    {
+        "machine_name":"Boiler Machine",
+        "machine_id":"machine004",
+        "current":0,
+        "voltage":0,
+        "power_factor":0 ,
+        "active_power": 0.0,
+        "apparent_power" :0.0 ,
+        "reactive_power": 0.0,
+        "daily_energy": 0,
+        "monthly_energy" :0,
+        "yearly_energy" : 0,
+        "idle_daily" : 0,
+        "idle_monthly" :0,
+        "idle_yearly" : 0 
     }
 ];
 
@@ -39,6 +39,7 @@ window.addEventListener('load', (event) => {
 console.log('page is fully loaded');
 constructTable('#table')
 });
+
 
 function constructTable(selector) {
 
@@ -56,6 +57,7 @@ for (var colIndex = 0; colIndex < cols.length; colIndex++)
     // with the column name
     if (val == null) val = "";
         row.append($('<td/>').html(val));
+        
 }
 
 // Adding each row to the table
@@ -66,21 +68,53 @@ $(selector).append(row);
 function Headers(list, selector) {
 var columns = [];
 var header = $('<tr/>');
+var energyHeader = $('<tr/>');
+//ar idleHeader = $('<tr/>');
 
 for (var i = 0; i < list.length; i++) {
 var row = list[i];
 
+var eneryRow = false;
+var idelRow = false;
+
 for (var k in row) {
     if ($.inArray(k, columns) == -1) {
         columns.push(k);
-        
+
+        if(k.includes("_energy")){
+            k = k.replace('_energy','').toUpperCase();
+            k = (k == "DAILY" ? "Today" : (k == "MONTHLY" ? "MTH" : "YTD"))
+
+            energyHeader.append($('<th/>').html(k));
+
+            if(!eneryRow){
+                header.append($('<th colspan="3"/>').html("ENERGY CONSUMED (kWh)"));
+                eneryRow = true;
+            }
+        }
+        else if(k.includes("idle_")){
+            k = k.replace('idle_','').toUpperCase();
+            k = (k == "DAILY" ? "Today" : (k == "MONTHLY" ? "MTH" : "YTD"))
+            energyHeader.append($('<th/>').html(k));
+
+            if(!idelRow){
+                header.append($('<th colspan="3"/>').html("IDLE TIME"));
+                idelRow = true;
+            }
+        }
+        else{
         // Creating the header
-        header.append($('<th/>').html(k));
+            header.append($('<th rowspan="2"/>').html(k.replace('_',' ').toUpperCase()));
+        }
+
     }
 }
 }
 
 // Appending the header to the table
 $(selector).append(header);
+$(selector).append(energyHeader);
+
+
 return columns;
 }	
